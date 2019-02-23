1ff2af2bd1eea60a50b6d021394a758d1e057190
Jason Brockett <jason.h.brockett@gmail.com>
2019-02-23 02:50:06 +0100

    Added Transformable

---
+++ Assets/Transformable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Scale(Vector3 scale)
+    {
+        transform.localScale = scale;
+    }
+
+    public void Rotate(float degrees)
+    {
+        transform.Rotate(Vector3.forward, degrees);
+    }
+
+    public void Move(Vector3 pos)
+    {
+        transform.position = pos;
+    }
+}
